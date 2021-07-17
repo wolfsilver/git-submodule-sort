c0cd1b1ebe318800089c6ea8bc392a050353a31a
roczhao <roczhao@tencent.com>
2021-07-17 18:24:01 +0800

    fix: patch mulit times

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 const vscode = require('vscode');
 const path = require('path');
-const fs = require("fs").promises;
+const fs = require("fs");
 const parse = require('@babel/parser').parse;
 const generate = require('@babel/generator').default;
 const template = require('@babel/template').default;
@@ -13,29 +13,51 @@ const patchFileName = 'workbench.desktop.main.patch.js';
 const sortRepositoriesBody = template.ast`let sortIndexs = visibleRepositories.map((repo, index) => ({ index, uri: repo.provider.rootUri.path, children: [] })); sortIndexs.sort((a, b) => b.uri.length - a.uri.length); for (let i = sortIndexs.length; i--;) { const repo = sortIndexs[i]; const rootUri = repo.uri + '/'; for (let j = i; j--;) { if (sortIndexs[j].uri.includes(rootUri)) { repo.children = sortIndexs.splice(j, 1).concat(repo.children); --i; } } } sortIndexs.sort((a, b) => a.index - b.index); sortIndexs = sortIndexs.reduce((acc, repo) => { acc.push(repo, ...repo.children); return acc; }, []); return sortIndexs.map(({ index }) => visibleRepositories[index]);`;
 
 let BASE_PATH = '';
+let step = 0;
 
 
-function getVSCodePath () {
+function getVSCodePath() {
 	const app = path.dirname(require.main.filename);
 	BASE_PATH = path.join(app, "vs", "workbench");
 }
 
 // backup
-function backup () {
-
+async function backup() {
+	const unWritable = await fs.promises.access(path.resolve(BASE_PATH, fileName), fs.constants.W_OK).catch(() => true);
+	if (unWritable) {
+		vscode.window.showErrorMessage('VS Code can not modify itself.');
+		return false;
+	}
+	const notBackup = await fs.promises.access(path.resolve(BASE_PATH, backUpFileName), fs.constants.F_OK).catch(e => {
+		console.log('backup file not exist', e);
+		return true;
+	});
+	if (notBackup) {
+		await fs.promises.copyFile(path.resolve(BASE_PATH, fileName), path.resolve(BASE_PATH, backUpFileName));
+	}
+	return true;
 }
 
-// body...
+// TODO
 function restore() {
 
 }
 
 
 async function patch() {
+	step = 0;
+	const status = await backup();
+	if (!status) {
+		return;
+	}
 	const config = vscode.workspace.getConfiguration("git-submodule-sort");
-	const data = await fs.readFile(path.resolve(BASE_PATH, fileName), 'utf8')
+	const data = await fs.promises.readFile(path.resolve(BASE_PATH, backUpFileName), 'utf8')
 	const ast = parse(data);
 	transformer(ast, config.prefix);
+	if (step !== 4) {
+		vscode.window.showErrorMessage('git submodule sort patch failed!!');
+		return;
+	}
 	const { code } = generate(ast, {
 		compact: true,
 		concise: true,
@@ -44,11 +66,14 @@ async function patch() {
 }
 
 async function output(data) {
-	await fs.writeFile(path.join(BASE_PATH, fileName), data).catch((e) => {
+	await fs.promises.writeFile(path.join(BASE_PATH, fileName), data).catch((e) => {
 		console.error(e)
 		vscode.window.showWarningMessage('git submodule sort patch failed!');
 	})
-	vscode.window.showInformationMessage('git submodule sort patch successed!');
+	vscode.window.showInformationMessage('git submodule sort patch successed! Restart to take effect.', { title: 'Restart VS Code' })
+		.then(() => {
+			vscode.commands.executeCommand("workbench.action.reloadWindow")
+		});
 }
 
 function transformer(ast, prefix = '┡') {
@@ -56,22 +81,24 @@ function transformer(ast, prefix = '┡') {
 		ClassMethod: function (path) {
 			if (path.node.key.name === 'renderElement') {
 				if (path.toString().includes('.provider.rootUri')) {
-					console.log('find renderElement')
 					let varVal = '';
 					path.traverse({
 						IfStatement: function (path) {
 							path.traverse({
 								VariableDeclarator: function (path) {
 									varVal = path.node.id.name;
-									console.log('find renderElement val', varVal)
 								},
 								CallExpression: function (path) {
 									// r.name.textContent = f.basename(a.provider.rootUri)
 									if (path.node.callee.type === 'MemberExpression' &&
 										path.node.callee.property.name === 'basename'
 									) {
-										path.replaceWith(t.binaryExpression('+', t.conditionalExpression(t.identifier(varVal), t.stringLiteral(`${prefix} `), t.stringLiteral('')), path.node))
-										path.skip()
+										if (!varVal) {
+											return;
+										}
+										path.replaceWith(t.binaryExpression('+', t.conditionalExpression(t.identifier(varVal), t.stringLiteral(`${prefix} `), t.stringLiteral('')), path.node));
+										path.skip();
+										step++;
 									}
 								}
 							});
@@ -80,7 +107,6 @@ function transformer(ast, prefix = '┡') {
 				}
 			}
 			if (path.node.kind === 'set' && path.node.key.name === 'visibleRepositories') {
-				console.log('find set visibleRepositories')
 				path.traverse({
 					SequenceExpression: function (path) {
 						path.traverse({
@@ -95,7 +121,8 @@ function transformer(ast, prefix = '┡') {
 											), [path.node.right]
 										)
 									)
-									path.skip()
+									path.skip();
+									step++;
 								}
 							}
 						})
@@ -104,7 +131,6 @@ function transformer(ast, prefix = '┡') {
 			}
 			if (path.node.key.name === 'onDidAddRepository') {
 				if (path.toString().includes('this._visibleRepositories.push')) {
-					console.log('find onDidAddRepository')
 					path.traverse({
 						SequenceExpression: function (path) {
 							path.traverse({
@@ -130,10 +156,10 @@ function transformer(ast, prefix = '┡') {
 												)]
 											)
 										)
-										path.parent.expressions.splice(1, 0, exp)
-										path.skip()
-										path.parentPath.skip()
-										// console.log('find this._visibleRepositories.push')
+										path.parent.expressions.splice(1, 0, exp);
+										path.skip();
+										path.parentPath.skip();
+										step++;
 									}
 								}
 							});
@@ -141,7 +167,8 @@ function transformer(ast, prefix = '┡') {
 					});
 
 					const sortRepositories = t.classMethod('method', t.identifier('sortRepositories'), [t.identifier('visibleRepositories')], t.blockStatement(sortRepositoriesBody))
-					path.insertAfter(sortRepositories)
+					path.insertAfter(sortRepositories);
+					step++;
 				}
 			}
 		},
@@ -154,9 +181,6 @@ function transformer(ast, prefix = '┡') {
  */
 function activate(context) {
 	getVSCodePath();
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "git-submodule-sort" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
@@ -165,7 +189,7 @@ function activate(context) {
 	context.subscriptions.push(patchInstall);
 }
 
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
